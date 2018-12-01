9fca4658668a544ce8e5700ca0387597a4a3c939
qgswsg <qgswsg@msn.cn>
2018-12-01 23:56:57 +0800

    为Demo中的控件着色

--- app/src/main/java/com/qgswsg/sideslipentry/MainActivity.java
+++ app/src/main/java/com/qgswsg/sideslipentry/MainActivity.java
@@ -1,15 +1,20 @@
 package com.qgswsg.sideslipentry;
 
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.qgswsg.sideslipentrybehaviorlib.SideslipEntryBehavior;
 
@@ -18,7 +23,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
+    private static final String TAG = "MainActivity";
+    private MyViewPager viewPager;
     private LinearLayout myLl;
 
     @Override
@@ -26,8 +32,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final List<View> viewList = new ArrayList<>();
-        viewList.add(View.inflate(this, R.layout.viewpager_item, null));
-        viewList.add(View.inflate(this, R.layout.viewpager_item, null));
+        View inflate = View.inflate(this, R.layout.viewpager_item, null);
+        CardView icv = inflate.findViewById(R.id.icv);
+        icv.setBackgroundColor(Color.BLUE);
+        viewList.add(inflate);
+        View inflate1 = View.inflate(this, R.layout.viewpager_item, null);
+        CardView icv1 = inflate1.findViewById(R.id.icv);
+        icv1.setBackgroundColor(Color.YELLOW);
+        viewList.add(inflate1);
         viewPager = findViewById(R.id.myVp);
         myLl = findViewById(R.id.myLl);
         myLl.post(new Runnable() {
@@ -47,6 +59,20 @@ public class MainActivity extends AppCompatActivity {
                 from.setSmallTailWidth(width);
             }
         });
+
+//        RecyclerView recyclerView = findViewById(R.id.myRv);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        List<String> items = new ArrayList<>();
+//        StringBuilder builder = new StringBuilder();
+//        for (int i = 0; i < 100; i++) {
+//            builder.delete(0, builder.length());
+//            builder.append("这是第").append(i).append("项");
+//            items.add(builder.toString());
+//        }
+//        recyclerView.setAdapter(new Adapter(items));
+
         viewPager.setAdapter(new PagerAdapter() {
             @Override
             public int getCount() {
@@ -70,5 +96,39 @@ public class MainActivity extends AppCompatActivity {
                 container.removeView(viewList.get(position));
             }
         });
+    }
+
+    class Adapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        private List<String> items;
+
+        public Adapter(List<String> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new MyViewHolder(View.inflate(MainActivity.this, android.R.layout.simple_list_item_1, null));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+            myViewHolder.textView.setText(items.get(i));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
     }
 }
